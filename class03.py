15f695f33ea05fa9ce12c29bbbad97df2e92ec4d
mini <itman390@gmail.com>
2022-03-20 21:08:26 +0900

    add class 3

---
+++ class03.py
@@ -0,0 +1,4 @@
+# in
+gold_box = ['foo', 'foo', 'gold', 'foo']
+if 'gold' in gold_box:
+    print("Yes")
--- project03.py
+++ project03.py
@@ -7,9 +7,8 @@
 i = 1
 thirty_one = [1, 3, 5, 7, 8, 10, 12]
 thirty = [4, 6, 9, 11]
-while i <= 12:
-# for i in range(1,13):
-    print("{0} 월은".format(i), end='')
+for i in range(1,13):
+    print("{0} 월은".format(i), end=' ')
     # 31일 까지 있는 경우
     if i in thirty_one:
         print("{0} 일까지 있습니다.".format(31))
@@ -19,4 +18,3 @@ while i <= 12:
     # 28일까지 있는 경우
     else:
         print("{0} 일까지 있습니다.".format(28))
-    i = i + 1
