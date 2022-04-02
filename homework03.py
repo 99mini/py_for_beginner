0d870f14fa75c0e285e66ce7f69a91b9fd5ed07c
mini <itman390@gmail.com>
2022-04-02 10:43:31 +0900

    add homework03

--- homework03.py
+++ homework03.py
@@ -16,8 +16,24 @@ print()
 #   ***
 #  *****
 
-for i in range(3):
-    for j in range(3-i):
+level = int(input())
+for i in range(level):
+    for j in range(level-i-1):
         print(' ',end='')
     stars = i*2+1
     print('*'*stars)
+
+# 별 찍기 3
+#      *
+#     * *
+#    * * *
+#   * * * *
+
+for i in range(level):
+    print(' ' * (level-i-1), end='')
+    for j in range(1,2*i+1):
+        if j % 2 ==0:
+            print(' ',end='')
+        else:
+            print('*',end='')
+    print()
