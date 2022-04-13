f9a8aac76882d7cf2927bd88c24d2b1ca65891ca
mini <itman390@gmail.com>
2022-04-13 16:18:46 +0900

    add stack

---
+++ algorithm/stack.py
@@ -0,0 +1,16 @@
+s = []
+
+# push
+s.append(1)
+s.append(2)
+s.append(3)
+
+# pop
+top = s.pop()
+print(f'top: {top}')
+print(f'stack: {s}')
+
+# top
+top = s[-1]
+print(f'top: {top}')
+print(f'stack: {s}')
