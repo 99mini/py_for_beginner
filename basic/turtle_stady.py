a083aa89897a3d2fa95a5f3015b0aaf2ee1f7a98
mini <itman390@gmail.com>
2022-04-26 20:51:17 +0900

    add comment object & class

--- basic/turtle_stady.py
+++ basic/turtle_stady.py
@@ -1,11 +1,50 @@
-from turtle import Turtle
+import turtle as t
+import math
 
-def draw(n):
-    angle = 180*(1-2/n)
-    for i in range(n):
-        Turtle.forward(100)
-        Turtle.right(180 - angle)
-    while True:
-        pass
-    
-draw(3)
+a = 50
+b = 50 * math.sqrt(3)
+c = 100
+
+t.screensize(1000,1000)
+
+t.circle(c)
+
+t.begin_fill()
+t.color("red")
+
+# 빗변
+t.left(90)
+t.forward(c*2)
+
+# 밑변
+t.right(120)
+t.forward(a*2)
+
+# 높이
+t.right(90)
+t.forward(b*2)
+
+t.end_fill()
+
+t.penup()
+t.backward(b)
+t.left(90)
+t.forward(b)
+t.left(90)
+t.pendown()
+
+t.circle(b)
+
+t.penup()
+t.left(90)
+t.forward(b)
+t.right(90)
+t.forward(b)
+t.left(90)
+t.forward(a)
+t.right(90)
+t.forward(a)
+t.left(90)
+t.pendown()
+
+t.circle(a)
