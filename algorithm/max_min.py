e319776ff4d1642b85e0518cf7df4f8db70f5040
mini <itman390@gmail.com>
2022-05-07 00:24:49 +0900

    add swap

--- algorithm/max_min.py
+++ algorithm/max_min.py
@@ -1,8 +1,9 @@
 # find max / min
 
-list1 = {1,3,2,9,10,20,1010,24}
+list1 = [1,3,2,9,10,20,1010,24]
 
 max_number = -1
+
 
 for i in range(len(list1)):
     if list1[i] > max_number:
---
+++ algorithm/swap.py
@@ -0,0 +1,11 @@
+#swap
+a = 20
+b = 30
+
+temp = a
+a = b
+b= temp
+
+a,b=b,a
+
+print(a,b)
