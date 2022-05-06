e0fd73c779449c31c156221256d919a40e478eab
mini <itman390@gmail.com>
2022-05-06 16:20:07 +0900

    add max/min algorithm

---
+++ algorithm/max_min.py
@@ -0,0 +1,12 @@
+# find max / min
+
+list1 = {1,3,2,9,10,20,1010,24}
+
+max_number = -1
+
+for i in range(len(list1)):
+    if list1[i] > max_number:
+        max_number = list1[i]
+
+print("max number = ", max_number)
+
