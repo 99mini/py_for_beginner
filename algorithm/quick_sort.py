c95929f1c2e539306fdc21cc1ec32fa4e8818f14
mini <itman390@gmail.com>
2022-06-10 20:12:07 +0900

    add quick sort

---
+++ algorithm/quick_sort.py
@@ -0,0 +1,32 @@
+import random
+
+
+def quick_sort(arr):
+    def sort(low, high):
+        if high <= low:
+            return
+
+        mid = partition(low, high)
+        sort(low, mid - 1)
+        sort(mid, high)
+
+    def partition(low, high):
+        pivot = arr[(low + high) // 2]
+        while low <= high:
+            while arr[low] < pivot:
+                low += 1
+            while arr[high] > pivot:
+                high -= 1
+            if low <= high:
+                arr[low], arr[high] = arr[high], arr[low]
+                low, high = low + 1, high - 1
+        return low
+
+    return sort(0, len(arr) - 1)
+
+arr = [random.randint(0,10000) for _ in range(100)]
+print("====== Init Arr ======")
+print(arr)
+quick_sort(arr)
+print("\n====== Sorted Arr ======")
+print(arr)
